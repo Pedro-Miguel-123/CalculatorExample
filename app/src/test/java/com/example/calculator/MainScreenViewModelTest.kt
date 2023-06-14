35404b10d5dbae70e2f9b6e278fdfc216fce75ce
Pedro Miguel Santos <pedrosantos1999@gmail.com>
2023-06-14 15:38:37 +0100

    fixed unit test

--- app/src/test/java/com/example/calculator/MainScreenViewModelTest.kt
+++ app/src/test/java/com/example/calculator/MainScreenViewModelTest.kt
@@ -5,34 +5,54 @@ import com.example.calculator.data.CalculatorRepository
 import com.example.calculator.data.Repository
 import com.example.calculator.nav.RouteNavigator
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
 class MainScreenViewModelTest {
 
    private val repository:  CalculatorRepository = mock()
    private val routeNavigator: RouteNavigator = mock()
-
+   lateinit var viewModel: MainScreenViewModel
+   @Before
+   fun setup() {
+      viewModel = createViewModel()
+   }
    @Test
-   fun setDigits() {
-      val viewModel = createViewModel()
+   fun setFirstDigitTest() {
       //Check everything is empty when we start
       Assert.assertEquals(viewModel.uiState.firstNumber, "")
-      Assert.assertEquals(viewModel.uiState.operationToDo, "")
-      Assert.assertEquals(viewModel.uiState.secondNumber, "")
       //Set some values
       viewModel.setFirstNumberDigit("5")
       Assert.assertEquals(viewModel.uiState.firstNumber, "5")
+   }
+
+   @Test
+   fun setOperationTest() {
+      setFirstDigitTest()
+      Assert.assertEquals(viewModel.uiState.operationToDo, "")
+
       viewModel.setOperation("*")
       Assert.assertEquals(viewModel.uiState.operationToDo, "*")
+   }
+
+   @Test
+   fun setSecondDigitTest() {
+      setOperationTest()
       viewModel.setSecondNumberDigit("3")
       Assert.assertEquals(viewModel.uiState.secondNumber, "3")
       viewModel.setSecondNumberDigit("0")
       //Second number should now be 30
       Assert.assertEquals(viewModel.uiState.secondNumber, "30")
+   }
+
+   @Test
+   fun verifyResult() {
+      setSecondDigitTest()
       // 5 * 30 = 150, lets check the result
       viewModel.setOperation("=")
-      Assert.assertEquals(repository.getResultValue(), 150)
+      verify(repository).setResultValue(150)
    }
 
    private fun createViewModel() = MainScreenViewModel(repository, routeNavigator)
